de60e965f0d9f1e682ae762c302b0aebbb0a34f5
AdetonaMichael <powerofelectron616@gmail.com>
2023-12-14 01:37:04 +0100

    Demonstration with Instance Variable and Function Parameters

--- GradeBook.java
+++ GradeBook.java
@@ -1,6 +1,21 @@
 public class GradeBook{
-    // method main Begains Execution of Java Program
-    public  void displayMessage(){
-       System.out.print("This is the Result from the GradeBook");
-    }
+  private String courseName;
+  private int courseCode;
+
+  public void setCourseName(String nameOfCourse){
+    courseName = nameOfCourse;
+  }
+  public void setCourseCode(int codeOfCourse){
+    courseCode = codeOfCourse;
+  }
+
+  public String getCourseName(){
+    return courseName;
+  }
+  public int getCourseCode(){
+    return courseCode;
+  }
+  public void showCourseInformation(){
+    System.out.printf("Welcome To Your Grade Book\n Your Course Title is: \n%s and your course code is \n%d", getCourseName(), getCourseCode());
+  }
 }
--- GradeBookTest.java
+++ GradeBookTest.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
 
 public class GradeBookTest{
     public static void main(String [] args){
+        Scanner courseInput = new Scanner(System.in);
+        System.out.println("Please Enter Your Course Title: ");
+        String usersCourse = courseInput.nextLine();
         GradeBook myGradeBook  = new GradeBook();
-        myGradeBook.displayMessage();
+        
+
+        
+        myGradeBook.setCourseName(usersCourse);
+        System.out.println("Student Course Set Successfully...");
+        myGradeBook.setCourseCode(202);
+        System.out.println("The Course Code has Been Set Successfully...");
+
+        System.out.println("Showing Message. ");
+        myGradeBook.showCourseInformation();
+
+        
+        courseInput.close();
     }
 }
